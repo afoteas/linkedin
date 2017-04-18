234ae0b1a59b27059853badfe6f88e61276b4f31
fwteas <afwteas@phys.uoa.gr>
2017-04-18 14:56:01 +0300

    init

--- UsingAsserts/src/com/lynda/javatraining/calc/Calculator2.java
+++ UsingAsserts/src/com/lynda/javatraining/calc/Calculator2.java
@@ -3,12 +3,16 @@ package com.lynda.javatraining.calc;
 public class Calculator2 {
 
 	public static void main(String[] args) {
+
+		assert true;
 		String s1 = InputHelper.getInput("Enter a numeric value: ");
+		assert checkInput(s1);
 		String s2 = InputHelper.getInput("Enter a numeric value: ");
+		assert checkInput(s2);
 		String op = InputHelper.getInput("Enter + or - or * or / ");
-		
+
 		double result = 0;
-		
+
 		switch (op) {
 		case "+":
 			result = MathHelper.addValues(s1, s2);
@@ -27,8 +31,17 @@ public class Calculator2 {
 			System.out.println("You entered an incorrect operator");
 			return;
 		}
-		
+
 		System.out.println("The answer is " + result);
 	}
-	
+	private static boolean checkInput(String s)
+	{
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+	}
 }
